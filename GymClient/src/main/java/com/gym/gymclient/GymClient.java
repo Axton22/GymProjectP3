0c1fb6cee0edda258075aab1425fa69dcf67bc43
Axton <axtonurbinaperez@gmail.com>
2025-10-21 22:34:02 -0600

    Primer commit, añadir proyectos API y Cliente

---
+++ GymClient/src/main/java/com/gym/gymclient/GymClient.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gym.gymclient;
+
+/**
+ *
+ * @author Axton Urbina
+ */
+public class GymClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
