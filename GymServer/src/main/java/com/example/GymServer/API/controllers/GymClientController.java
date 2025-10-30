367fdd6dfe1f7fa523977d0626ee72cfffe1d747
Axton <axtonurbinaperez@gmail.com>
2025-10-30 14:04:06 -0600

    Merge a rama main y modulo de servidor terminado

--- GymServer/src/main/java/com/example/GymServer/API/controllers/GymClientController.java
+++ GymServer/src/main/java/com/example/GymServer/API/controllers/GymClientController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +35,12 @@ public class GymClientController {
     
     @PostMapping()
     public GymClientEntity saveClient(@RequestBody GymClientEntity client) {
+        return this.gymClientService.saveClient(client);
+    }
+    
+    @PutMapping("/{id}")
+    public GymClientEntity updateClient(@PathVariable("id") Integer id, @RequestBody GymClientEntity client) {
+        client.setClientID(id);
         return this.gymClientService.saveClient(client);
     }
     
--- GymServer/src/main/java/com/example/GymServer/API/controllers/GymRoutineController.java
+++ GymServer/src/main/java/com/example/GymServer/API/controllers/GymRoutineController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +35,12 @@ public class GymRoutineController {
     
     @PostMapping()
     public GymRoutineEntity saveRoutine(@RequestBody GymRoutineEntity routine) {
+        return this.gymRoutineService.saveRoutine(routine);
+    }
+    
+    @PutMapping("/{id}")
+    public GymRoutineEntity updateRoutine(@PathVariable("id") Integer id, @RequestBody GymRoutineEntity routine) {
+        routine.setRoutineID(id);
         return this.gymRoutineService.saveRoutine(routine);
     }
     
